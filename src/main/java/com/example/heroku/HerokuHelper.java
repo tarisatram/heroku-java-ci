0f936928eaf18689e33d9b368f5473cb6b8108de
Taris Atram <taris.atram@outlook.com>
2016-11-19 19:55:04 -0500

    Updated the getAssignedPort method to use a variable for the default port.
    DEFAULT_PORT is a final constant. Better than hardcoding the port
    number in the default return statement of the method.

--- src/main/java/com/example/heroku/HerokuHelper.java
+++ src/main/java/com/example/heroku/HerokuHelper.java
@@ -8,10 +8,15 @@ public class HerokuHelper {
      * @return The port for the server to listen on.
      */
     public static int getAssignedPort() {
+        // Default TCP port to use when deploying locally.
+        final int DEFAULT_PORT = 4567;
+
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+
+        // return DEFAULT_PORT if the PORT environment variable isn't set.
+        return DEFAULT_PORT;
     }
 }
